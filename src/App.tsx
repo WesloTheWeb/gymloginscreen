5d51184c8c84e3935f7b047541c80e221ee391d6
WesloTheWeb <wesleyadwebster@gmail.com>
2025-08-19 23:59:33 -0500

    re-ordered the import statements

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
+import Header from './components/Header/Header';
+import GameMenu from './components/GameMenu/GameMenu';
+import Footer from './components/Footer/Footer';
 import './App.scss'
-import Footer from './components/Footer/Footer'
-import GameMenu from './components/GameMenu/GameMenu'
-import Header from './components/Header/Header'
 
 function App() {
   return (
