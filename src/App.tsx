eadf9078dbb943b0807ad46e10b849aea5f62441
WesloTheWeb <wesleyadwebster@gmail.com>
2025-08-19 23:56:13 -0500

    fixed pathing

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,8 @@ import './App.scss'
 import Footer from './components/Footer/Footer'
 import GameMenu from './components/GameMenu/GameMenu'
 import Header from './components/Header/Header'
-import backgroundImage from './assets/background.jpg'
 
 function App() {
-  // Set CSS custom property for background image
-  document.documentElement.style.setProperty('--background-image', `url(${backgroundImage})`);
-
   return (
     <>
       <Header />
